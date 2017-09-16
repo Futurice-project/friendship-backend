d16790097f0e71ac791624f6fcb9c239225f6387
Hieu Cao <qhieu45@gmail.com>
2017-09-16 10:47:27 +0300

    Add constraints to personalities & user_personality tables

--- db/migrations/005_add_table_personalities.js
+++ db/migrations/005_add_table_personalities.js
@@ -1,24 +1,32 @@
 /**
  * Add table for personalities
  * And create tabe user-personalities (allows a many to many relation)
+ * Add constrant levelCheck for level (only allows 1-5)
  */
 exports.up = knex =>
   knex.schema
     .createTableIfNotExists('personalities', (table) => {
       table.increments('id').primary();
-      table.text('name');
+      table.text('name').notNullable();
     })
     .createTableIfNotExists('user-personality', (table) => {
       table.increments('id').primary();
-      table.integer('userId').unsigned().references('id').inTable('users');
-      table.integer('personalityId').unsigned().references('id').inTable('personalities');
+      table
+          .integer('userId')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('users');
+      table
+          .integer('personalityId')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('personalities');
       // check constraint for level?? raw query?
-      table.integer('level').unsigned();
-    });
-
-// add check constraint later
-// exports.up = knex =>
-//   knex.raw('ALTER TABLE "user-personality" ADD constraint levelCheck CHECK (level > 0 AND level < 6)');
+      table.integer('level').notNullable();
+    }).then(() =>
+      knex.raw('ALTER TABLE "user-personality" ADD constraint levelCheck CHECK (level > 0 AND level < 6)'));
 
 exports.down = knex =>
   knex.schema
