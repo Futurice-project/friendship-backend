cec0bdda9939667a160bfa81e70c6af915d2e550
richardsnyderyp <richardsnyderyp@gmail.com>
2017-11-08 23:15:38 +0200

    Select more infor for chatroom

--- src/models/chatrooms.js
+++ src/models/chatrooms.js
@@ -1,45 +1,35 @@
 import knex from '../utils/db';
 
+const selectChatroomQuery =
+    `SELECT chatrooms.id as chatroomId, 
+      (SELECT json_build_object('id', id, 'username', username, 'emoji', emoji) 
+              FROM users WHERE id = user_creator_id) as creator, 
+      (SELECT json_build_object('id', id, 'username', username, 'emoji', emoji) 
+              FROM users WHERE id = user_receiver_id) as receiver,
+      ARRAY_AGG(row_to_json(messages)) as messages
+    FROM chatrooms JOIN messages ON chatrooms.id = messages.chatroom_id`;
 
 export const dbGetAllMsWithChatroomId = chatroomId =>
-knex('chatrooms')
-.innerJoin('messages', 'chatrooms.id', 'messages.chatroom_id')
-.select([
-  'chatrooms.id as chatroomId',
-  'user_creator_id',
-  'user_receiver_id',
-  knex.raw('ARRAY_AGG(row_to_json(messages)) as messages')])
-.groupBy('chatrooms.id')
-.where({ chatroom_id: chatroomId });
+knex.raw(`${selectChatroomQuery}
+          WHERE chatroom_id = ${chatroomId}
+          GROUP BY chatrooms.id;`)
+.then(results => results.rows);
 
 export const dbGetChatrooms = () =>
-knex('chatrooms')
-.innerJoin('messages', 'chatrooms.id', 'messages.chatroom_id')
-.select([
-  'chatrooms.id as chatroomId',
-  'user_creator_id',
-  'user_receiver_id',
-  knex.raw('ARRAY_AGG(row_to_json(messages)) as messages')])
-.groupBy('chatrooms.id');
-
+knex.raw(`${selectChatroomQuery}
+          GROUP BY chatrooms.id;`)
+.then(results => results.rows);
 
 export const dbGetChatroomsByUserId = userId =>
-knex('chatrooms')
-.innerJoin('messages', 'chatrooms.id', 'messages.chatroom_id')
-.select([
-  'chatrooms.id as chatroomId',
-  'user_creator_id',
-  'user_receiver_id',
-  knex.raw('ARRAY_AGG(row_to_json(messages)) as messages')])
-.where('user_creator_id', userId)
-.orWhere('user_receiver_id', userId)
-.groupBy('chatrooms.id');
+knex.raw(`${selectChatroomQuery}
+          WHERE user_receiver_id = ${userId} OR user_creator_id = ${userId}
+          GROUP BY chatrooms.id;`)
+.then(results => results.rows);
 
 export const dbGetAllMsFromChatrooms = () =>
 knex('chatrooms')
 .join('messages', 'chatrooms.id', 'messages.chatroomId')
 .join('users as u1', 'messages.userId', 'u1.id')
-//.join('users as u2', 'chatrooms.userReceiverId', 'u2.id')
 .select('messages.chatroomId', 'messages.userId as userCreatorId', 'u1.username as userCreatorName', 'messages.textMessage', 'messages.chatTime')
 .orderBy('chatrooms.id', 'asc');
 
