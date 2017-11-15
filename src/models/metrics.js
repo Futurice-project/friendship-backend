9fbf306f8b7bbae0ae1e12d63489b1a341109135
Eran <e.machiels@hotmail.com>
2017-11-15 13:52:41 +0200

    Added selecting registered user and inserting it in the metrics table

--- src/models/metrics.js
+++ src/models/metrics.js
@@ -1,4 +1,5 @@
 import knex from '../utils/db';
+import moment from 'moment';
 
 export const dbGetNbMatchesMessaging = () => {
   return -1;
@@ -14,4 +15,39 @@ export const dbGetNbMessages = () => {
 
 export const dbGetNbActiveUsers = () => {
   return -1;
+}
+
+export const dbGetUsersRegistered = () => {
+  return knex.raw(`SELECT COUNT(users.id) as users_count, (SELECT COUNT(users.id) FROM "users" WHERE "users"."createdAt" >= to_timestamp(${moment().startOf('day').unix()}) AND "users"."createdAt" <= to_timestamp(${moment().endOf('day').unix()})) as registered_today from users`).then(results => results.rows);
+}
+
+export const dbInsertUsersRegistered = (data) => {
+
+  const fields = {
+    users_count: parseInt(data.users_count, 0),
+    registered_today: parseInt(data.registered_today, 0),
+    timestamp: moment().utc().toISOString()
+  }
+
+  // knex('metrics_users_registered')
+  //     .insert(fields)
+  //     .returning('*');
+
+  return knex.transaction((tx) => {
+    return tx.insert(fields)
+      .into('metrics_users_registered')
+      .then((id) => {
+        console.log("User registration for today inserted");
+
+        return id;
+      })
+      .catch((err) => {
+        console.log("Insert failed", err);
+        throw err;
+      })
+  })
+    .catch((err) => {
+      console.log("Huh! Transaction failed!", err);
+      throw err;
+    });
 }
